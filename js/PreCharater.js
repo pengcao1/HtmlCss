4de833eb3abd1225605c6bbb2861066cb4287a6c
Peng.Cao <pcao@thoughtworks.com>
2018-10-23 21:16:27 +0800

    use `split` `Math.ceil` `replace` `eval` method to deal string

--- js/PreCharater.js
+++ js/PreCharater.js
@@ -3,7 +3,9 @@ function testFunction () {
     console.log("This is the testFunction..");
     const preStr = document.getElementById("inputStr").value;
     const preWith = document.getElementById("inputNum").value;
-    const strReturn =  main(preStr,Number(preWith));
+    const testStr = "The main theme of education in engineering school is learning to teach yourself";
+    const testWith = 30;
+    const strReturn = main(testStr, Number(testWith));
     console.log("result str = ", strReturn);
     document.getElementById("outputResult").value= strReturn;
 }
@@ -29,13 +31,36 @@ function main (PreStr,preWith) {
     } else if (!isValidStr(PreStr)) {
         return "ERROR: Invalid character detected!"
     }
-    return formatString(PreStr);
+    return formatString(PreStr, preWith);
 }
 
-function formatString(preStr) {
+function formatString(preStr, preWith) {
     // 以空格split 字符串(研究JavaScript中split的是实现方式)
     // 当有多个空格时如何处理？
     // 当处理的当前字符串，或者空格的index 在with之间？
-    return preStr;
+    var cutSubStr = "";
+    for (i = 0; i < Math.ceil((preStr.length / preWith )); i++ ) {
+        var itemStr = preStr.substring(i * preWith, (i + 1) * preWith);
+        // k = 0;
+        // for (j =0 ; j<itemStr.length ; j++) {
+        //     if (isAlphabet(itemStr.charAt(j))) {
+        //         k = j;
+        //     }
+        // }
+        var itemChar = itemStr.split(" ");
+        console.log("itemStr = >" + itemStr + "<");
+        var aa = itemStr.split(" ").toString().replace(eval("/"+","+"/g"),'('+(i+1)+'); ');
+        console.log("aa = >" + aa + "<");
+        //cutSubStr += preStr.substring(i*preWith,(i+1)*preWith);
+        cutSubStr += aa;
+    }
+    return cutSubStr + "(" + Math.ceil((preStr.length / preWith))+")";
+}
+
+function isAlphabet(item) {
+    return (item >= 'a' && item <= 'z') || (item >= 'A' && item <= 'Z') ;
+}
+function isSpace(item) {
+    return item === ' ';
 }
 
