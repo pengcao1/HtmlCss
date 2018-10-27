d17ab3ea69e3d45c1f7e5e5c43f608c2bf07f906
Peng.Cao <pcao@thoughtworks.com>
2018-10-27 20:22:11 +0800

    finish the homework

--- js/dealCharater.js
+++ js/dealCharater.js
@@ -28,4 +28,45 @@ school is learning to teach yourself who is your";
     // storage = [...storage, buff_str];
     storage.push(buff_str)
     console.log(">>>", storage);
+
+    storage_new =[];
+    for (let i=0; i< storage.length;i++) {
+        storage_new[i] = [];
+    }
+    console.log(storage_new);
+
+    var buff_count = 0;
+    const line_length = 20;
+    var cnt = 1;
+    storage.map((words, index) => {
+        buff_count +=words.length;
+        if (buff_count < line_length) {
+            storage_new[index].push(cnt);
+        } else if(buff_count === line_length) {
+            buff_count = 0;
+            storage_new[index].push(cnt);
+            cnt+=1;
+        } else {
+            var x = Math.trunc(buff_count/line_length);
+            var y = buff_count % line_length;
+            for(let i=0 ; i< (x+1) ; i++){
+                storage_new[index].push(cnt + i);
+            }
+            buff_count = y;
+            cnt += x;
+        }
+        //console.log(words +"<>"+index);
+    })
+
+
+    var out_str = "";
+    // 输出
+    storage.map((words, index) => {
+        out_str += words + "(";
+        for(let i=0; i< (storage_new[index].length-1); i++){
+            out_str += storage_new[index][i]+",";
+        }
+        out_str += storage_new[index][storage_new[index].length - 1] + ");"
+    });
+    console.log(out_str);
 }
