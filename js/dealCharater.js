cfa05caaba2ca8d59403da91f3b9c843d9a28dbf
Peng.Cao <pcao@thoughtworks.com>
2018-10-27 22:19:54 +0800

    add string valid judge and width

--- js/dealCharater.js
+++ js/dealCharater.js
@@ -1,16 +1,32 @@
 const main = () => {
-    console.log("==");
     var g_OutPut = [];
+    var g_OutStr = "";
     const str_test = "The     main theme of education in engineering    \
-school is learning to teach yourself who is your";
+school is learning to teach yourself who is your     ";
     const str_WIDTH = 20;
 
+    if (!(result = isValidInput(str_test, str_WIDTH)).result){
+        console.log(result.msg);
+        return;
+    }
 
     splitStringToItem(g_OutPut,str_test);
-
-    console.log(formatOutput(g_OutPut, str_WIDTH));
-
+    g_OutStr = formatOutput(g_OutPut, str_WIDTH);
+    console.log(g_OutStr);
 }
+
+isValidInput = (STR, WIDTH) => {
+  if (STR === undefined || STR === null || STR === "") return {result:false,msg:"Empty String"};
+  var pattern = /[^a-zA-Z|\s]/;
+  if (isNaN(WIDTH)) {
+      return { result: false, msg: "ERROR: not a Number, please input number between [10,80]" };
+  } else if (!(WIDTH < 81 && WIDTH > 10)) {
+      return { result: false, msg: "ERROR: with out of range" };
+  } else if (pattern.test(STR)) {
+      return { result: false, msg: "ERROR: Invalid character detected!" };
+  }
+    return { result: true, msg: "" };
+};
 
 formatOutput = (g_OutPut, str_WIDTH) => {
   var bufferCurrentLine = 0;
