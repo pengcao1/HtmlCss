d2d6ecb91bdf7550e7b2d64500cdc14fb20dbd00
Peng.Cao <pcao@thoughtworks.com>
2018-10-30 22:20:42 +0800

    删除固定的行， 输出特定的行

--- js/dealCharater.js
+++ js/dealCharater.js
@@ -1,61 +1,81 @@
 const main = () => {
     var g_OutPut = [];
     var g_OutStr = "";
-    const str_test = "The main theme of education in engineering school is learning to teach yourself";
+    //const str_test = "The main theme of education in engineering school is learning to teach yourself";
     //const str_test = "So   many whitespaces";
-    const str_WIDTH = 30;
-    // const str_test = document.getElementById("inputStr").value;
-    // const str_WIDTH = document.getElementById("inputNum").value;
-
+    //const str_WIDTH = 30;
+    // step 1 get input
+    const str_test = document.getElementById("inputStr").value;
+    const str_WIDTH = parseInt(document.getElementById("inputNum").value);
 
     const inputDetailDel = document.getElementById("inputDetailDel").open;
     const inputDetailOut = document.getElementById("inputDetailOut").open;
 
+    //step 2 format input string & judge input is correct
     console.log("inputDetailDel=" + inputDetailDel + ":inputDetailOut=" + inputDetailOut);
     if (!(result = isValidInput(str_test, str_WIDTH)).result) {
       console.log(result.msg);
       document.getElementById("outputResult").value = result.msg;
       return;
     }
-    splitStringToItem(g_OutPut, str_test);
+    g_OutPut = splitStringToItem(g_OutPut, str_test);
     formatOutput(g_OutPut, str_WIDTH);
 
+    // step 3 re-format output info
     if (inputDetailDel) {
-      g_OutStr = formatOutDelLine(g_OutPut, parseInt(document.getElementById("inputNumDel").value));
+      var del_Output = [];
+      var formatDelStringObj = formatOutDelLine(g_OutPut, parseInt(document.getElementById("inputNumDel").value))
+      if (formatDelStringObj.status) {
+        g_OutStr = formatDelStringObj.message;
+        del_Output = splitStringToItem(del_Output, g_OutStr);
+        formatOutput(del_Output, str_WIDTH);
+        g_OutStr = formatOutputAll(del_Output);
+      } else {
+        g_OutStr = formatDelStringObj.message;
+      }
     } else if (inputDetailOut) {
-      g_OutStr = formatOutSpecificLine(g_OutPut, parseInt(document.getElementById("inputNumOut").value));
+      g_OutStr = formatOutSpecificLine(g_OutPut, parseInt(document.getElementById("inputNumOut").value)).message;
     } else {
       g_OutStr = formatOutputAll(g_OutPut);
     }
 
+    // step 4 output the info the UI
     console.log(g_OutStr);
     document.getElementById("outputResult").value = g_OutStr;
 }
 
 formatOutSpecificLine = (g_OutPut, lineNumber) => {
+  if (g_OutPut[g_OutPut.length - 1].index < lineNumber) {
+     return { status: false, message: "ERROR: Line number out of range" };
+  }
   var out="";
   console.log("formatOutSpecificLine, lineNumber = ", lineNumber);
   g_OutPut.map( (item,index) => {
     out += (item.index === lineNumber) ? item.str : "";
   })
-  return out;
+  return { status: true, message: out };
 };
 
-formatOutDelLine = (g_OutPut, delLineNumber) => {
-
+formatOutDelLine = (l_OutPut, delLineNumber) => {
+  if (l_OutPut[l_OutPut.length-1].index < delLineNumber) {
+    return { status: false, message: "ERROR: Line number out of range" };
+  }
+  var out = [];
+  console.log("formatOutDelLine, delLineNumber = ", delLineNumber);
+  l_OutPut.map((item, index) => {
+    out += item.index !== delLineNumber ? item.str : "";
+  });
+  return { status: true, message:out};
 };
 
-formatOutputAll = g_OutPut => {
-  // console.log(g_OutPut);
+formatOutputAll = l_OutPut => {
+  // console.log(l_OutPut);
   var g_str = "";
-  for (let i = 0; i < g_OutPut.length; i++) {
-    g_str += g_OutPut[i].outPut();
+  for (let i = 0; i < l_OutPut.length; i++) {
+    g_str += l_OutPut[i].outPut();
   }
   return g_str;
 };
-
-
-
 
 isValidInput = (STR, WIDTH) => {
   if (STR === undefined || STR === null || STR === "") return {result:false,msg:"Empty String"};
@@ -70,25 +90,25 @@ isValidInput = (STR, WIDTH) => {
     return { result: true, msg: "" };
 };
 
-formatOutput = (g_OutPut, str_WIDTH) => {
+formatOutput = (l_OutPut, str_WIDTH) => {
   var bufferCurrentLine = 0;
   var bufferInLine = 1;
-  for (let i = 0; i < g_OutPut.length; i++) {
-    bufferCurrentLine += g_OutPut[i].str.length;
+  for (let i = 0; i < l_OutPut.length; i++) {
+    bufferCurrentLine += l_OutPut[i].str.length;
     if (bufferCurrentLine < str_WIDTH) {
-      g_OutPut[i].split.push(bufferInLine);
-      g_OutPut[i].index = bufferInLine; // the first line index
+      l_OutPut[i].split.push(bufferInLine);
+      l_OutPut[i].index = bufferInLine; // the first line index
     } else if (bufferCurrentLine === str_WIDTH) {
       bufferCurrentLine = 0;
-      g_OutPut[i].split.push(bufferInLine);
-      g_OutPut[i].index = bufferInLine; // the first line index
+      l_OutPut[i].split.push(bufferInLine);
+      l_OutPut[i].index = bufferInLine; // the first line index
       bufferInLine += 1;
     } else {
       var numberOfMultiLine = Math.trunc(bufferCurrentLine / str_WIDTH);
       var nextLineOffset = bufferCurrentLine % str_WIDTH;
-      g_OutPut[i].index = bufferInLine; // the first line index
+      l_OutPut[i].index = bufferInLine; // the first line index
       for (let j = 0; j <= numberOfMultiLine; j++) {
-        g_OutPut[i].split.push(bufferInLine + j);
+        l_OutPut[i].split.push(bufferInLine + j);
       }
       bufferCurrentLine = nextLineOffset;
       bufferInLine += numberOfMultiLine;
@@ -96,7 +116,7 @@ formatOutput = (g_OutPut, str_WIDTH) => {
   }
 };
 
-splitStringToItem = (g_OutPut,str_test) => {
+splitStringToItem = (l_OutPut,str_test) => {
     var str_item = "";
     const pattern = /\s/;
     for (let i = 0; i < str_test.length; i++) {
@@ -108,13 +128,14 @@ splitStringToItem = (g_OutPut,str_test) => {
         if (pattern.test(element) === isStartOrEndWithSpace) {
           str_item += element;
         } else {
-          g_OutPut.push(new FormatString(g_OutPut.length, str_item, []));
+          l_OutPut.push(new FormatString(l_OutPut.length, str_item, []));
           str_item = element;
           isStartOrEndWithSpace = pattern.test(element);
         }
       }
     }
-    g_OutPut.push(new FormatString(g_OutPut.length, str_item, []));
+    l_OutPut.push(new FormatString(l_OutPut.length, str_item, []));
+    return l_OutPut;
 }
 
 class FormatString {
